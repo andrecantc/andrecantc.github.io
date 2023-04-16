eb15445b03730cb967a2508f04c67e95b781c609
André Gaudet <anga@localhost.localdomain>
2023-04-16 23:45:34 +0200

    test

---
+++ src/portfolio.js
@@ -0,0 +1,33 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  title: 'AG.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'André Gaudet',
+  role: 'IT Student',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: 'https://drive.google.com/file/d/1e3S2iDVx9bjf_CCrA_rp0nFa8YclRCDL/view?usp=share_link',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+  },
+}
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'andre.gaudet.ag@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
